8e8f08b286658452acf35b6db8d5fd21201b164e
Grzegorz Zaręba <mobile443@gmail.com>
2018-01-25 21:33:02 +0100

    Interface

--- TrailAnalyzer/TrailAnalyzer/Interfaces/ITimeAnalyzer.cs
+++ TrailAnalyzer/TrailAnalyzer/Interfaces/ITimeAnalyzer.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using TrailAnalyzer.Models;
 
 namespace TrailAnalyzer.Interfaces
 {
-    interface ITimeAnalyzer
+    public interface ITimeAnalyzer
     {
+        double TotalTrackTime(Trail trail);
+        double ClimnbingTime(Trail trail);
+        double DescentTime(Trail trail);
+        double FlatTime(Trail trail);
     }
 }
